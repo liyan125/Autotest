614a1f80554b25acf3a30442e6d4fb5187840d9d
liyan125 <liyyan851029@163.com>
2020-09-08 11:55:31 +0800

    重新提交

--- Chapter6/src/main/java/com/test/extend/demo/TestMethodDemo.java
+++ Chapter6/src/main/java/com/test/extend/demo/TestMethodDemo.java
@@ -1,2 +1,24 @@
-package com.test.extend.demo;public class TestMethodDemo {
-}
+package com.test.extend.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+    public class TestMethodDemo{
+        @Test
+        public void test1(){
+            Assert.assertEquals(1,2);
+        }
+
+        @Test
+        public void test2(){
+            Assert.assertEquals("aaa","aaa");
+        }
+
+        @Test
+        public void logdemo(){
+            Reporter.log("这是我们自己写的日志");
+            throw new RuntimeException("这是我自己的运行时异常");
+        }
+
+    }
--- Chapter6/src/main/java/com/test/extend/demo/test1.java
+++ Chapter6/src/main/java/com/test/extend/demo/test1.java
@@ -1,2 +1,10 @@
-package com.test.extend.demo;public class test1 {
+package com.test.extend.demo;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+    @Test
+    public void testcase1() {
+        System.out.println("这是测试用例1");
+    }
 }
